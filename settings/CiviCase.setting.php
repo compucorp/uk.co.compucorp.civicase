e35f5d05740c4c40f257338e92af2d45efd414ee
René Olivo <rene@compucorp.co.uk>
2020-05-12 00:57:45 +0100

    CATL-1348: Add allow linked cases page setting

--- settings/CiviCase.setting.php
+++ settings/CiviCase.setting.php
@@ -21,6 +21,21 @@ $setting = [
     'description' => 'This will allow cases to be locked for certain contacts.',
     'help_text' => '',
   ],
+  'civicaseAllowLinkedCasesPage' => [
+    'group_name' => 'CiviCRM Preferences',
+    'group' => 'core',
+    'name' => 'civicaseAllowLinkedCasesPage',
+    'type' => 'Boolean',
+    'quick_form_type' => 'YesNo',
+    'default' => FALSE,
+    'html_type' => 'radio',
+    'add' => '4.7',
+    'title' => 'Allow linked cases page',
+    'is_domain' => 1,
+    'is_contact' => 0,
+    'description' => '',
+    'help_text' => '',
+  ],
 ];
 
 $caseSetting = new CRM_Civicase_Service_CaseCategorySetting();
