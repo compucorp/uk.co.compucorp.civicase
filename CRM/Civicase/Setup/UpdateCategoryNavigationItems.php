d36d2f429b2dc32c346270915992e1f2020e2680
Tano Rojas <lisandro@compucorp.co.uk>
2021-04-28 23:19:30 +0700

    CPS-576: Add upgrader for all the categories

--- CRM/Civicase/Setup/UpdateCategoryNavigationItems.php
+++ CRM/Civicase/Setup/UpdateCategoryNavigationItems.php
@@ -1,12 +1,7 @@
 <?php
 
-use CRM_Civicase_Helper_CaseCategory as CaseCategoryHelper;
-
 /**
  * Class for updating menus of Case Categories dynamically created.
- *
- * These are marked as "not reserved" ('is_reserved' == 0) and have all the
- * same set of items, according to instance type. *
  */
 class CRM_Civicase_Setup_UpdateCategoryNavigationItems {
 
@@ -14,32 +9,27 @@ class CRM_Civicase_Setup_UpdateCategoryNavigationItems {
    * Update category navigation items.
    */
   public function apply() {
-    // Update menu corresponding to not reserved case categories.
-    $categories = $this->getNotReservedCategories();
+    $categories = civicrm_api3('OptionValue', 'get', [
+      'option_group_id' => 'case_type_categories',
+    ])['values'];
 
     foreach ($categories as $category) {
-      $categoryInstance = CaseCategoryHelper::getInstanceObject($category['value']);
-      $categoryMenu = $categoryInstance->getMenuObject();
-      $categoryMenu->resetCaseCategorySubmenusUrl($category['name']);
+      $items = civicrm_api3('Navigation', 'get', [
+        'sequential' => 1,
+        'url' => ['LIKE' => "%case_type_category={$category['name']}%"],
+      ])['values'];
+
+      foreach ($items as $item) {
+        civicrm_api3('Navigation', 'get', [
+          'id' => $item['id'],
+          'api.Navigation.create' => [
+            'id' => '$value.id',
+            'url' => str_ireplace($category['name'], $category['value'], $item['url']),
+          ],
+        ]);
+      }
     }
 
     CRM_Core_BAO_Navigation::resetNavigation();
   }
-
-  /**
-   * Get categories with `is_reserved` equal to 0.
-   */
-  private function getNotReservedCategories() {
-    $categories = civicrm_api3('OptionValue', 'get', [
-      'option_group_id' => 'case_type_categories',
-      'is_reserved' => 0,
-    ]);
-
-    if ($categories['count'] === 0) {
-      return [];
-    }
-
-    return $categories['values'];
-  }
-
 }
